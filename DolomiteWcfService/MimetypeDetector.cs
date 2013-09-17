957e48de8448f2887ce8667d4eb2384521ec1056
Benjamin Russell <benrr101@csh.rit.edu>
2013-09-17 00:22:57 -0400

    A memory-inefficient method of detecting mimetypes, but it solves the problem with ID3 tagged flac files. And it makes more sense. Fixes #2

--- DolomiteWcfService/MimetypeDetector.cs
+++ DolomiteWcfService/MimetypeDetector.cs
@@ -1,11 +1,15 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Text;
 
 namespace DolomiteWcfService
 {
-    class MimetypeDetector
+    internal class MimetypeDetector
     {
+        private const string FlacBytes = "fLaC";
+
         /// <summary>
         /// Mapping of mimetype to signature. If it isn't in this list, it isn't supported.
         /// </summary>
@@ -19,9 +23,9 @@ namespace DolomiteWcfService
             // MP3 w/ID3v2 tags
             {new byte[] {0x49, 0x44, 0x33}, "audio/mpeg"},
             // WMA
-            { new byte[] {0x30, 0x26, 0xB2, 0x75, 0x8E, 0x66, 0xCF, 0x11, 0xA6, 0xD9, 0x00, 0xAA, 0x00, 0x62, 0xCE, 0x6C}, "audio/x-ms-wma" },
+            {new byte[] {0x30, 0x26, 0xB2, 0x75, 0x8E, 0x66, 0xCF, 0x11, 0xA6, 0xD9, 0x00, 0xAA, 0x00, 0x62, 0xCE, 0x6C}, "audio/x-ms-wma" },
             // M4A Apple Lossless
-            { new byte[] {0x00, 0x00, 0x00, 0x20, 0x66, 0x74, 0x79, 0x70, 0x4D, 0x34, 0x41, 0x20, 0x00, 0x00, 0x00, 0x00}, "audio/mp4a-latm" },
+            {new byte[] {0x00, 0x00, 0x00, 0x20, 0x66, 0x74, 0x79, 0x70, 0x4D, 0x34, 0x41, 0x20, 0x00, 0x00, 0x00, 0x00}, "audio/mp4a-latm" },
             // Flac
             {new byte[] {0x66, 0x4C, 0x61, 0x43}, "audio/x-flac"},
             // MP2 LC-AAC
@@ -31,7 +35,7 @@ namespace DolomiteWcfService
             // Astrid/Quartex AAC
             {new byte[] {0x41, 0x41, 0x43, 0x00, 0x01, 0x00}, "audio/aac"},
             // OGG Vorbis
-            { new byte[] {0x4F, 0x67, 0x67, 0x53, 0x00, 0x02, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00}, "audio/ogg" },
+            {new byte[] {0x4F, 0x67, 0x67, 0x53, 0x00, 0x02, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00}, "audio/ogg"},
             // Wav
             {new byte[] {0x52, 0x49, 0x46, 0x46}, "audio/wav"}
         };
@@ -54,14 +58,14 @@ namespace DolomiteWcfService
         /// <summary>
         /// Whitespace hex values. Useful for detecting multipart parser fuck-ups.
         /// </summary>
-        private static readonly byte[] WhitespaceBytes = new byte[]
-            {
-                0x09,
-                0x0A,
-                0x0B,
-                0x0C,
-                0x0D
-            };
+        private static readonly char[] WhitespaceBytes =
+        {
+            (char) 0x09,
+            (char) 0x0A,
+            (char) 0x0B,
+            (char) 0x0C,
+            (char) 0x0D
+        };
 
         /// <summary>
         /// Fetches the file extension that goes with the requested mimetype
@@ -80,33 +84,34 @@ namespace DolomiteWcfService
         /// <returns>A string mimetype on successful lookup. Null otherwise</returns>
         public static string GetMimeType(FileStream stream)
         {
+            // Read the file into a string
+            byte[] bytes = new byte[stream.Length];
+            stream.Read(bytes, 0, Convert.ToInt32(stream.Length));
+            string streamString = Encoding.Default.GetString(bytes);
+            streamString = streamString.TrimStart(WhitespaceBytes);
+
             // Iterate over the available audio types
             foreach (var audioType in AudioTypes)
             {
-                // Reset the stream to the beginning
-                stream.Position = 0;
-                
-                // Compare all bytes in the header of the stream to the type's header
-                bool match = true;
-                foreach (byte b in audioType.Key)
+                // Turn the bytes into a string
+                string typeString = Encoding.Default.GetString(audioType.Key);
+
+                // The header of the file must be the 
+                bool match = streamString.StartsWith(typeString);
+
+                // If it matches, we need to check for mp3 vs flac
+                if (match)
                 {
-                    byte sb = (byte)stream.ReadByte();
-
-                    // Skip whitespace bytes (if multipart parser messes up)
-                    if (WhitespaceBytes.Contains(b))
-                        continue;
-
-                    // If the byte doesn't match, then it's not a match
-                    if (b != sb)
+                    if (audioType.Value == "audio/mpeg")
                     {
-                        match = false;
-                        break;
+                        if (streamString.Contains(FlacBytes))
+                            return "audio/x-flac";
+                    }
+                    else
+                    {
+                        return audioType.Value;
                     }
                 }
-
-                // If we find a match, return it
-                if (match)
-                    return audioType.Value;
             }
 
             // If we make it to here, we've exhausted our possibilities
