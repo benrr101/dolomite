1e7e0379e4afa1bf2c5148fc4195f5787afbe898
Benjamin Russell <benrr101@csh.rit.edu>
2013-10-12 15:55:15 -0400

    Stubbing out the playlist endpoint

---
+++ DolomiteWcfService/IPlaylistEndpoint.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.IO;
+using System.ServiceModel;
+using System.ServiceModel.Channels;
+using System.ServiceModel.Web;
+
+namespace DolomiteWcfService
+{
+    [ServiceContract]
+    interface IPlaylistEndpoint
+    {
+
+        #region Create Methods
+
+        [OperationContract]
+        [WebInvoke(Method = "POST", UriTemplate = "/")]
+        Message CreatePlaylist(Stream body);
+
+        #endregion
+
+        #region Retrieve Methods
+
+        [OperationContract]
+        [WebInvoke(Method = "GET", UriTemplate = "/")]
+        Message GetAllPlaylists();
+
+
+        [OperationContract]
+        [WebInvoke(Method = "GET", UriTemplate = "/{guid}")]
+        Message GetPlaylist(string guid);
+
+        #endregion
+
+        #region Update Methods
+
+        [OperationContract]
+        [WebInvoke(Method = "POST", UriTemplate = "/{guid}/")]
+        Message AddToPlaylist(Stream body, string guid);
+
+        #endregion
+
+        #region Delete Methods
+
+        [OperationContract]
+        [WebInvoke(Method = "DELETE", UriTemplate = "/{guid}/{id}")]
+        Message DeleteFromPlaylist(string guid, string id);
+
+        #endregion
+
+    }
+}
---
+++ DolomiteWcfService/PlaylistEndpoint.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.ServiceModel.Channels;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DolomiteWcfService
+{
+    class PlaylistEndpoint : IPlaylistEndpoint
+    {
+        public Message CreatePlaylist(Stream body)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Message GetAllPlaylists()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Message GetPlaylist(string guid)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Message AddToPlaylist(Stream body, string guid)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Message DeleteFromPlaylist(string guid, string id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
