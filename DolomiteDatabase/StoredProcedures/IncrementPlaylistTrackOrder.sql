0ba9c7da0f48559eabde8889dd06b08d30b8ccca
Benjamin Russell <benrr101@csh.rit.edu>
2013-10-14 22:17:55 -0400

    Adding on update/delete triggers to playlist tracks FKs Also adding a new stored proc that allows for insertion of a playlist track into the middle of the playlist

---
+++ DolomiteDatabase/StoredProcedures/IncrementPlaylistTrackOrder.sql
@@ -0,0 +1,11 @@
+﻿CREATE PROCEDURE [dbo].[IncrementPlaylistTrackOrder]
+	@playlist UNIQUEIDENTIFIER,
+	@position INT
+AS
+	UPDATE [PlaylistTracks] 
+		SET [Order] = [Order] + 1
+		WHERE 
+			[Order] >= @position
+			AND [Playlist] = @playlist
+
+RETURN 0
