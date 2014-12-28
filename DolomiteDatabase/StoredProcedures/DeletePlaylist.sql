77d5b1c83b2598d6435602c4425e74d42cf2230f
Benjamin Russell <benrr101@outlook.com>
2014-12-28 15:28:13 -0800

    Removing FK actions that were causing cyclic cascades. Replacing with sprocs that will take care of deletions

---
+++ DolomiteDatabase/StoredProcedures/DeletePlaylist.sql
@@ -0,0 +1,25 @@
+﻿CREATE PROCEDURE [dbo].[DeletePlaylist]
+	@playlistGuid UNIQUEIDENTIFIER
+AS
+BEGIN TRANSACTION
+	-- Find the ID of the playlist to be deleted
+	DECLARE @playlistId BIGINT = (
+		SELECT [Id] 
+		FROM [Playlists] 
+		WHERE [GuidId] = @playlistGuid
+	)
+
+	-- Make sure there is only one playlist selected
+	IF COUNT(@playlistId) <> 1
+	BEGIN
+		RETURN 0
+	END
+
+	-- Delete all the tracks in the playlist
+	DELETE FROM [PlaylistTracks] WHERE [Playlist] = @playlistId
+
+	-- Delete the playlist, itself
+	DELETE FROM [Playlists] WHERE [Id] = @playlistId
+
+COMMIT
+RETURN 1
---
+++ DolomiteDatabase/StoredProcedures/DeleteTrack.sql
@@ -0,0 +1,31 @@
+﻿CREATE PROCEDURE [dbo].[DeleteTrack]
+	@trackGuid UNIQUEIDENTIFIER
+AS
+BEGIN TRANSACTION
+	-- Find the ID of the track to be deleted
+	DECLARE @trackId BIGINT = (
+		SELECT [Id] 
+		FROM [Tracks] 
+		WHERE [GuidId] = @trackGuid
+	)
+
+	-- Make sure there is only one track selected
+	IF COUNT(@trackId) <> 1
+	BEGIN
+		RETURN 0
+	END
+
+	-- Delete any references to the track in playlists
+	DELETE FROM [PlaylistTracks] WHERE [Track] = @trackId
+
+	-- Delete the track's metadata
+	DELETE FROM [Metadata] WHERE [Track] = @trackId
+
+	-- Delete all records of the track in the quality table
+	DELETE FROM [AvailableQualities] WHERE [Track] = @trackId
+
+	-- Delete the track itself
+	DELETE FROM [Tracks] WHERE [Id] = @trackId
+
+COMMIT
+RETURN 1
