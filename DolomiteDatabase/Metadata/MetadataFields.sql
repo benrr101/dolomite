17dc0a93f29c8ad5a761338c7fb5651960b67a2e
Benjamin Russell <benrr101@outlook.com>
2016-02-21 22:31:32 -0800

    Revamping the metadata fields

--- DolomiteDatabase/Metadata/MetadataFields.sql
+++ DolomiteDatabase/Metadata/MetadataFields.sql
@@ -1,30 +1,33 @@
 ﻿-- Add the supported metadata fields to the database
 MERGE INTO MetadataFields AS [target]
 USING ( VALUES
-	(1, 'Title', 'Title', 'string', ((1)), ((1)), ((0))), 
-	(2, 'Artist', 'Artist', 'string', ((1)), ((1)), ((1))), 
-	(3, 'AlbumArtist', 'Album Artist', 'string', ((1)), ((1)), ((1))),
-	(4, 'Composer', 'Composer', 'string', ((1)), ((1)), ((1))),
-	(5, 'Album', 'Album', 'string', ((1)), ((1)), ((0))),
-	(6, 'Genre', 'Genre', 'string', ((1)), ((1)), ((1))),
-	(7, 'Year', 'Year', 'numeric', ((0)), ((1)), ((0))),
-	(8, 'Track', 'Track Number', 'numeric', ((1)), ((1)), ((0))),
-	(9, 'TrackCount', 'Track Count', 'numeric', ((0)), ((1)), ((0))),
-	(10, 'Disc', 'Disc Number', 'numeric', ((0)), ((1)), ((0))),
-	(11, 'Lyrics', 'Lyrics', 'string', ((0)), ((1)), ((0))),
-	(12, 'BeatsPerMinute', 'BPM', 'numeric', ((0)), ((1)), ((0))),
-	(13, 'Conductor', 'Conductor', 'string', ((1)), ((1)), ((0))),
-	(14, 'Copyright', 'Copyright', 'string', ((1)), ((1)), ((0))),
-	(15, 'Comment', 'Comment', 'string', ((1)), ((1)), ((0))),
-	(16, 'DiscCount', 'Disc Count', 'numeric', ((0)), ((1)), ((0))),
-	(17, 'DateAdded', 'Date Added', 'date', ((0)), ((0)), ((0))),
-	(18, 'PlayCount', 'Play Count', 'numeric', ((0)), ((0)), ((0))),
-	(19, 'LastPlayed', 'Last Played', 'date', ((0)), ((0)), ((0))),
-	(20, 'Duration', 'Duration', 'numeric', ((0)), ((0)), ((0))),
-	(21, 'TrackListing', 'Track Listing', 'string', ((1)), ((0)), ((0))),
-	(22, 'OriginalBitrate', 'Original Bitrate', 'numeric', ((0)), ((0)), ((0))),
-	(23, 'OriginalFormat', 'Original Format', 'numeric', ((0)), ((0)), ((0)))
-	) AS [source] ([Id], [TagName], [DisplayName], [Type], [Searchable], [FileSupported], [TagLibArray])
+	-- File supported metadata fields
+	(1,		'Artist',		'Artist',		'string',	((1)), ((1))), 
+	(2,		'AlbumArtist',	'Album Artist', 'string',	((1)), ((1))),
+	(3,		'Album',		'Album',		'string',	((1)), ((1))),
+	(4,		'Composer',		'Composer',		'string',	((1)), ((1))),
+	(5,		'Performer',	'Performer',	'string',	((1)), ((1))),
+	(6,		'Date',			'Date',			'numeric',	((0)), ((1))),
+	(7,		'Genre',		'Genre',		'string',	((1)), ((1))),
+	(8,		'Title',		'Title',		'string',	((1)), ((1))), 
+	(9,		'DiscNumber',	'Disc Number',	'numeric',	((0)), ((1))),
+	(10,	'TotalDiscs',	'Total Discs',	'numeric',	((0)), ((1))),
+	(11,	'TrackNumber',	'Track Number', 'numeric',	((0)), ((1))),
+	(12,	'TotalTracks',	'Total Tracks', 'numeric',	((0)), ((1))),
+	(13,	'Copyright',	'Copyright',	'string',	((1)), ((1))),
+	(14,	'Comment',		'Comment',		'string',	((1)), ((1))),
+
+	(15,	'DurationMilli','Duration',		'numeric',	((0)), ((0))),
+	
+	-- Dolomite-specific metadata fields
+	(16,	'Dol:DateAdded',		'Date Added',		'date',		((0)), ((0))),
+	(17,	'Dol:LastPlayed',		'Last Played',		'date',		((0)), ((0))),
+	(18,	'Dol:PlayCount',		'Play Count',		'numeric',	((0)), ((0))),
+	(19,	'Dol:SkipCount',		'Skip Count',		'numeric',	((0)), ((0))),
+	(20,	'Dol:TrackListing',		'TrackListing',		'string',	((1)), ((0))),
+	(21,	'Dol:OriginalBitrate',	'Original Bitrate',	'numeric',	((0)), ((0))),
+	(22,	'Dol:OriginalCodec',	'Original Codec',	'string',	((0)), ((0)))
+	) AS [source] ([Id], [TagName], [DisplayName], [Type], [Searchable], [FileSupported])
 ON [source].[id] = [target].[id]
 WHEN MATCHED THEN
 	UPDATE SET 
@@ -32,11 +35,10 @@ WHEN MATCHED THEN
 		[target].[DisplayName] = [source].[DisplayName],
 		[target].[Type] = [source].[Type],
 		[target].[Searchable] = [source].[Searchable],
-		[target].[FileSupported] = [source].[FileSupported],
-		[target].[TagLibArray] = [source].[TagLibArray]
+		[target].[FileSupported] = [source].[FileSupported]
 WHEN NOT MATCHED BY TARGET THEN
-	INSERT ([Id], [TagName], [DisplayName], [Type], [Searchable], [FileSupported], [TagLibArray])
-	VALUES ([Id], [TagName], [DisplayName], [Type], [Searchable], [FileSupported], [TagLibArray])
+	INSERT ([Id], [TagName], [DisplayName], [Type], [Searchable], [FileSupported])
+	VALUES ([Id], [TagName], [DisplayName], [Type], [Searchable], [FileSupported])
 WHEN NOT MATCHED BY SOURCE THEN
 	DELETE;
 
