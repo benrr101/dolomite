67f24bc23659a5b04e2f96872ec7c7eff742b383
Benjamin Russell <benrr101@outlook.com>
2016-03-18 21:33:47 -0700

    partial commit of in progress changes in branch

---
+++ DolomiteBackgroundProcessing/ArtWriting.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DolomiteManagement;
+
+namespace DolomiteBackgroundProcessing
+{
+    class ArtWriting
+    {
+        #region Properties
+
+        public const string LocalTrackStorageDirectory = "artwork";
+
+        public const string WorkCheckIntervalKey = "Art_WorkCheckInterval";
+
+        /// <summary>
+        /// Amount of time to sleep in between checks for work
+        /// </summary>
+        public static TimeSpan WorkCheckInterval { private get; set; }
+
+        /// <summary>
+        /// The container in Azure storage that's holding the tracks
+        /// </summary>
+        public static string TrackStorageContainer { private get; set; }
+
+        #endregion
+
+        #region Start/Stop Logic
+
+        private volatile bool _shouldStop;
+
+        /// <summary>
+        /// Sets the stop flag on the thread loop
+        /// </summary>
+        public void Stop()
+        {
+            _shouldStop = true;
+        }
+
+        #endregion
+
+        public void Run()
+        {
+            
+            LocalStorageManager.Instance.InitializeStorageDirectory(LocalTrackStorageDirectory);
+
+        }
+
+    }
+}
